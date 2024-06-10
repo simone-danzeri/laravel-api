20eebea2fd6a784d220480a1c8c2225b1e2bb993
simone-danzeri <s.danzeri@icloud.com>
2024-06-10 13:20:48 +0200

    feat: milestone 1/2: created API for all projects

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index() {
+        $projects = Project::with('technologies', 'type')->get();
+        return response()->json([
+            "success" => true,
+            "results" => $projects
+        ]);
+    }
+}
