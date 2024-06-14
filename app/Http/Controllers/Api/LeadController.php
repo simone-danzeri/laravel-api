d98341486845618eac6d7e63273600019b9594fb
simone-danzeri <s.danzeri@icloud.com>
2024-06-14 09:23:49 +0200

    feat: new lead on submit with validation

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -4,11 +4,29 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Lead;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
     public function store(Request $request) {
         $data = $request->all();
-
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'email' => 'required|email',
+            'message' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+        $newLead = new Lead();
+        $newLead->fill($data);
+        $newLead->save();
+        return response()->json([
+            'success' => true
+        ]);
     }
 }
